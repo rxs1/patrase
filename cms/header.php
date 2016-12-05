790b36608d393c988a5787e318e02ff5c88a37ea
claraind <clara.ara7@gmail.com>
2016-12-05 15:35:00 +0700

    done manage gambar pasar, modify logo admin ke dashboard, add some common function

--- cms/header.php
+++ cms/header.php
@@ -15,7 +15,7 @@
   <header class="main-header">
 
     <!-- Logo -->
-    <a href="index2.html" class="logo">
+    <a href="dashboard.php" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>P</b>A </span>
       <!-- logo for regular state and mobile devices -->
