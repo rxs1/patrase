53edc36d344dd8d33f72afae728fbb47c6e28610
dewi <srdewii22@gmail.com>
2016-12-12 11:32:03 +0700

    add map with current location

--- route.php
+++ route.php
@@ -135,7 +135,7 @@ http://www.tooplate.com/view/2075-digital-team
 
     <div class="row">
       <div class="col-md-12 col-sm-12" >
-        <h3>Insert map heree....</h3>
+        <div id="map" style="width:100%;height:600px;"></div>
       </div>
     </div>
     
@@ -163,6 +163,40 @@ http://www.tooplate.com/view/2075-digital-team
 <script src="js/wow.min.js"></script>
 <script src="js/custom.js"></script>
 <script type="text/javascript">
+
+   function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 15
+        });
+        var infoWindow = new google.maps.InfoWindow({map: map});
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+      }
   $(document).ready(function(){
 
     if (sessionStorage.getItem("id_pasar")) {
@@ -245,6 +279,9 @@ http://www.tooplate.com/view/2075-digital-team
 
   });
 </script>
+<script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC-CkwZplHgj3yDPctt_PKmaAR56SsGbd8&callback=initMap">
+    </script>
 
 </body>
 </html>
